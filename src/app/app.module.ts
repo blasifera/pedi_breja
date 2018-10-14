75431693d67cf1877a5502fd901bca4a574148e3
Lucas Blasifera <lucasblasifera@hotmail.com>
2018-10-14 19:42:06 -0300

    Foi criado um servico para controlar o login (user.service.ts) e configurado para o firebase, inicialmente para login pelo google.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuthModule } from 'angularfire2/auth';
 import { AppComponent } from './app.component';
+import { AngularFireDatabaseModule } from 'angularfire2/database'
 import { ProdutosComponent } from './produtos/produtos.component';
 
 @NgModule({
@@ -10,7 +12,19 @@ import { ProdutosComponent } from './produtos/produtos.component';
     ProdutosComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(
+      {
+        apiKey: "AIzaSyDGpeaKYGFXdmvJUaEYfA05KM3AlSqiuIc",
+        authDomain: "pedi-breja.firebaseapp.com",
+        databaseURL: "https://pedi-breja.firebaseio.com",
+        projectId: "pedi-breja",
+        storageBucket: "pedi-breja.appspot.com",
+        messagingSenderId: "200440626130"
+      }
+    ),
+    AngularFireAuthModule,
+    AngularFireDatabaseModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
