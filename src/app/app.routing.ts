e916933f44116d9fe7659409de66afacf490a361
Lucas Blasifera <lucasblasifera@hotmail.com>
2018-10-21 23:33:22 -0300

    rotas e teste de login

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,10 +2,13 @@ import { Routes, RouterModule } from '@angular/router'
 import { ModuleWithProviders } from '@angular/compiler/src/core';
 import { ProdutosComponent } from './produtos/produtos.component';
 import { CarrinhoComponent } from './carrinho/carrinho.component';
+import { LoginComponent } from './login/login.component';
 
 const APP_ROUTES: Routes = [
-    { path: '', component: ProdutosComponent },
-    { path: 'carrinho', component: CarrinhoComponent}
+    { path: '', component: LoginComponent },
+    { path: 'carrinho', component: CarrinhoComponent},
+    { path: 'produtos', component: ProdutosComponent}
+
 
 ];
 
